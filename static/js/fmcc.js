bd2ae5bc86de896464fb9501074f6386a1be0c29
Anna Warzecha <anna.warzecha@gmail.com>
2015-11-15 20:58:41 +0100

    fmcc is back to life woohooo! ✨

---
+++ static/js/fmcc.js
@@ -0,0 +1,78 @@
+/*jslint browser: true*/ /*global $*/
+(function() {
+    "use strict";
+    var loadColor = function(item) {
+        $("#colorName")
+            .data("ui-autocomplete")
+            ._trigger("select", "autocompleteselect", {
+                item: {
+                    "value": item.colorcode,
+                    "label": item.colorname,
+                    "defaultColor": item.defaultcolor
+                }
+            });
+    };
+
+    var parseColors = function(data) {
+        return $.map(data, function(item) {
+            return {
+                "value": item.colorcode,
+                "label": item.colorname,
+                "defaultColor": item.defaultcolor
+            };
+        });
+    };
+
+    var prepareCloud = function(data) {
+        var words = data.sort(function() {
+            return Math.random();
+        });
+        words = words.slice(0, 15);
+        var cloud = $("#cloud");
+        $(words).each(function(index, item) {
+            var a = $(
+                    "<a />", {
+                        href: "#",
+                        title: "szukano " + item.clicks + " razy",
+                        text: item.colorname + " ",
+                        dataColorcode: item.colorcode
+                    }
+                )
+                .css("color", item.colorcode)
+                .css("font-size", Math.random() * (30 - 12) + 12)
+                .click(function(e) {
+                    e.preventDefault();
+                    loadColor(item);
+                });
+            cloud.append(a);
+        });
+    };
+
+    var prepareColorBox = function(item) {
+        $("#loading").show();
+        $("#colorName").val(item.label);
+        $("#colorBox").css("background-color", item.value);
+        $("#colorCode").text(item.value);
+        $("#defaultColor").text(item.defaultColor);
+        $("#loading").fadeOut("slow");
+        $("#result").show();
+    };
+
+    $().ready(function() {
+        var colors;
+        $.getJSON("db/colors_pl.json").done(function(data) {
+            colors = parseColors(data);
+            prepareCloud(data);
+        });
+        $("#colorName").autocomplete({
+            source: function(request, response) {
+                var filteredArray = $.ui.autocomplete.filter(colors, request.term);
+                response(filteredArray.slice(0, 10));
+            },
+            select: function(e, ui) {
+                e.preventDefault();
+                prepareColorBox(ui.item);
+            }
+        });
+    });
+}());
